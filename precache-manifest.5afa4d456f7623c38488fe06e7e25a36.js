3f78cf8bcf76a968f3a5ace3f7afbc974021df52
Hangover-Escape <wjt17600279551@163.com>
2022-01-26 15:58:53 +0800

    deploy

---
+++ precache-manifest.5afa4d456f7623c38488fe06e7e25a36.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886f138d040df178aa5b",
+    "url": "/vue-listProjects/css/about.2cc012a1.css"
+  },
+  {
+    "revision": "3cacbab394fcceff562a",
+    "url": "/vue-listProjects/css/app.4efa2aa6.css"
+  },
+  {
+    "revision": "503112f46722d952b770",
+    "url": "/vue-listProjects/css/chunk-vendors.6d88a643.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-listProjects/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-listProjects/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "f6c0aceaf072b23d6aa9e31e41587866",
+    "url": "/vue-listProjects/index.html"
+  },
+  {
+    "revision": "886f138d040df178aa5b",
+    "url": "/vue-listProjects/js/about.345af1d1.js"
+  },
+  {
+    "revision": "3cacbab394fcceff562a",
+    "url": "/vue-listProjects/js/app.b81a03f1.js"
+  },
+  {
+    "revision": "503112f46722d952b770",
+    "url": "/vue-listProjects/js/chunk-vendors.969a754e.js"
+  },
+  {
+    "revision": "d6031d41fec005495f01928a8f01d075",
+    "url": "/vue-listProjects/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-listProjects/robots.txt"
+  }
+]);
